3250822ac361672907aed59f9d8f3f84891a4754
Tanapoom Srikaewkheaw <tanapoomts1350@gmail.com>
2024-05-04 23:07:01 +0700

    Update home page.

--- home.py
+++ home.py
@@ -1,5 +1,8 @@
+"""My home page."""
+
 import tkinter as tk
 from tkinter import ttk
+
 
 class HomePage:
     def __init__(self, root):
@@ -17,20 +20,24 @@ class HomePage:
         button_style.configure("TButton", font=("Helvetica", 14))
 
         # Add a label for the title
-        title_label = ttk.Label(self.frame, text="Welcome to Future Laptop Price Advisor", font=("Helvetica", 18, "bold"))
+        title_label = ttk.Label(self.frame, text="Welcome to Future Laptop Price Advisor",
+                                font=("Helvetica", 18, "bold"))
         title_label.grid(row=0, column=0, columnspan=2, pady=20)
 
         # Add buttons with custom styling
-        search_filter_button = ttk.Button(self.frame, text="Search and Filter", command=self.go_to_filter_page, style="TButton")
+        search_filter_button = ttk.Button(self.frame, text="Search and Filter", command=self.go_to_filter_page,
+                                          style="TButton")
         search_filter_button.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
 
-        comparison_button = ttk.Button(self.frame, text="Comparison Tool", command=self.go_to_comparison_page, style="TButton")
+        comparison_button = ttk.Button(self.frame, text="Comparison Tool", command=self.go_to_comparison_page,
+                                       style="TButton")
         comparison_button.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
 
         rating_button = ttk.Button(self.frame, text="User Ratings", command=self.go_to_rating_page, style="TButton")
         rating_button.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
 
-        visualization_button = ttk.Button(self.frame, text="Statistics", command=self.go_to_visualization_page, style="TButton")
+        visualization_button = ttk.Button(self.frame, text="Statistics", command=self.go_to_visualization_page,
+                                          style="TButton")
         visualization_button.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
 
         exit_button = ttk.Button(self.frame, text="Exit", command=self.exit, style="TButton")
